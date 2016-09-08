daf0db6445edf14943781811abc4051b0ed466be
Flavio Andrade <flavio.machado@bcash.com.br>
2016-09-08 12:48:18 -0300

    conclui projeto

--- Javascript/Alura/Programando na linguagem da web/projeto/adiciona.js
+++ Javascript/Alura/Programando na linguagem da web/projeto/adiciona.js
@@ -1,9 +1,25 @@
-var pacienteNovo = "<tr class='paciente'>" +
-						"<td class='info-nome'>Flavio</td>" +
-						"<td class='info-peso'>71</td>" +
-						"<td class='info-altura'>1.71</td>" +
+var botaoAdicionar = document.querySelector('#adicionar-paciente');
+botaoAdicionar.addEventListener('click', function(event) {
+
+	event.preventDefault();
+
+	var nome = document.querySelector('#campo-nome');
+	var peso = document.querySelector('#campo-peso');
+	var altura = document.querySelector('#campo-altura');
+
+	var pacienteNovo = "<tr class='paciente'>" +
+						"<td class='info-nome'>"+ nome.value +"</td>" +
+						"<td class='info-peso'>"+ peso.value +"</td>" +
+						"<td class='info-altura'>"+ altura.value +"</td>" +
 						"<td class='info-imc'></td>" +
 					"</tr>";
 
-var todosPacientes = document.querySelector('table');
-todosPacientes.innerHTML += pacienteNovo;
+	var todosPacientes = document.querySelector('table');
+	todosPacientes.innerHTML += pacienteNovo;
+
+	var formPaciente = document.querySelector('form');
+	formPaciente.reset();
+
+	var botaoCalculaImc = document.querySelector('#calcula-imcs');
+	botaoCalculaImc.click();
+});
