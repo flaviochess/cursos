cf655360b8b49ecd5928050a7900ac9ccf32234a
Flavio Andrade <flavio.machado@bcash.com.br>
2016-10-27 15:39:08 -0200

    inicio de exercícios da aula 2

--- Nodejs/alura_nodejs_inovando_com_javascript_no_backend/casadocodigo/app.js
+++ Nodejs/alura_nodejs_inovando_com_javascript_no_backend/casadocodigo/app.js
@@ -1,11 +1,5 @@
-var express = require('express');
-var app = express();
-
-app.set('view engine', 'ejs');
-
-app.get('/produtos', function (req, res) {
-	res.render('produtos/lista.ejs')
-});
+var app = require('./config/express')();
+var rotasProdutos = require('./app/routes/produtos')(app);
 
 app.listen(3000, function() {
 	console.log('servidor rodando');
